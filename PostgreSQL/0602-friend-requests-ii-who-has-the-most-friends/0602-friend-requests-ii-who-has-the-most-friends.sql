b4190ed5d6b52b23bb84779c96f2090463972352
Hari Prasanna Ravichandran <154921668+Hari-prasanna@users.noreply.github.com>
2026-06-08 23:05:59 +0200

    06-08-2026 0602-friend-requests-ii-who-has-the-most-friends Database

---
+++ PostgreSQL/0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,15 @@
+WITH AllConnections AS (
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id FROM RequestAccepted
+)
+SELECT 
+    id, 
+    COUNT(*) AS num
+FROM 
+    AllConnections
+GROUP BY 
+    id
+ORDER BY 
+    num DESC
+LIMIT 1;
